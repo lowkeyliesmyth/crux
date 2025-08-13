158c2b7395659eb060e1d4597de739a30a651856
lowkey <lowkey@kaffeinlabs.com>
2025-08-13 08:51:38 -0700

    Bootstrap initial CLI boilerplate

--- src/crux.cr
+++ src/crux.cr
@@ -2,6 +2,27 @@ require "cling"
 require "colorize"
 require "yaml"
 
+require "./commands/base"
+Colorize.on_tty_only!
+
 module Crux
   VERSION = "0.1.0"
+
+  class Main < Commands::Base
+    def setup : Nil
+      @name = "crux"
+      @description = <<-DESC
+      The CLI tool at the crux of your day to day developer experience.
+      DESC
+
+      add_usage "crux <command> <subcommand> [options] <arguments>"
+
+      # TODO register commands here
+    end
+
+    def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+      # Show help menu by default if no other commands passed
+      stdout.puts help_template
+    end
+  end
 end
