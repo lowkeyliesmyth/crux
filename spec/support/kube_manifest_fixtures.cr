d68b7d9e38e6419f6cecd43d05d8f52aa44ebffe
lowkey <lowkey@kaffeinlabs.com>
2026-09-01 15:03:18 -0700

    test(kubemanifestfixtures): copy and alphasort all existing manifest fixtures

--- spec/support/kube_manifest_fixtures.cr
+++ spec/support/kube_manifest_fixtures.cr
@@ -1,9 +1,29 @@
 module KubeManifestFixtures
-  VALID_SINGLE_DOC = <<-YML
-    apiVersion: apps/v1
-    kind: Deployment
+  BACKSLASH_NAME_DOC = <<-YML
+    apiVersion: v1
+    kind: ConfigMap
     metadata:
-      name: my-app
+      name: "foo\\\\bar"
+    YML
+
+  CONFIGMAP_MULTILINE_DOC = <<-'YML'
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: shield-cluster
+    data:
+      cluster-shield.yaml: "cluster_config:\n  name: foo\n"
+    YML
+
+  LEADING_DOT_NAME_DOC = <<-YML
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: .hidden
+    YML
+
+  MALFORMED_YAML = <<-YML
+    key: [unclosed bracket
     YML
 
   MISSING_API_VERSION_DOC = <<-YML
@@ -12,15 +32,15 @@ module KubeManifestFixtures
       name: my-app
     YML
 
-  MISSING_METADATA_DOC = <<-YML
-    apiVersion: apps/v1
-    kind: Deployment
-    YML
-
   MISSING_KIND_DOC = <<-YML
     apiVersion: apps/v1
     metadata:
       name: orphan
+    YML
+
+  MISSING_METADATA_DOC = <<-YML
+    apiVersion: apps/v1
+    kind: Deployment
     YML
 
   MISSING_NAME_DOC = <<-YML
@@ -30,6 +50,24 @@ module KubeManifestFixtures
       namespace: default
     YML
 
+  MIXED_DOC = <<-YML
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: my-app
+    ---
+    apiVersion: v1
+    metadata:
+      name: my-app
+    YML
+
+  NULL_UNI_NAME_DOC = <<-'YML'
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: "foo\u0000bar"
+    YML
+
   NULL_NAME_DOC = <<-YML
     apiVersion: apps/v1
     kind: Deployment
@@ -37,7 +75,36 @@ module KubeManifestFixtures
       name:
     YML
 
-  MALFORMED_YAML = <<-YML
-    key: [unclosed bracket
+  SLASH_NAME_DOC = <<-YML
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: foo/bar
+    YML
+
+  TRAVERSAL_NAME_DOC = <<-YML
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: ../../../tmp/pwned
+    YML
+
+  VALID_MULTI_DOC = <<-YML
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: my-app
+    ---
+    apiVersion: v1
+    kind: Service
+    metadata:
+      name: my-app
+    YML
+
+  VALID_SINGLE_DOC = <<-YML
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: my-app
     YML
 end
